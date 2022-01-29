f813fe66474e6442eaf2db966455f209cb5db40c
Mike Avgeros <mike.avgeros@gmail.com>
2022-01-29 23:46:16 +0000

    Setup Django

---
+++ api/core/urls.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('djoser.urls')),
+    path('api/', include('djoser.urls.authtoken')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
