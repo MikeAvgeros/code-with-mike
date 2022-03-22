425af9595333aa2facc6dc9bf5814477ab169bd6
Mike Avgeros <mike.avgeros@gmail.com>
2022-03-22 19:22:29 +0000

    Add real text in about section. Add social media icons in footer.

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Box, Container } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
 const Footer = () => {
   const Copyright = () => {
@@ -27,6 +30,32 @@ const Footer = () => {
       }}
     >
       <Container maxWidth="sm">
+        <Grid container justifyContent="center" alignItems="center">
+          <a
+            href="https://facebook.com"
+            aria-label="Go to Facebook page"
+            target="_blank"
+            rel="noopener"
+          >
+            <FacebookIcon className="footer-icon" />
+          </a>
+          <a
+            href="https://twitter.com"
+            aria-label="Go to Twitter page"
+            target="_blank"
+            rel="noopener"
+          >
+            <TwitterIcon className="footer-icon" />
+          </a>
+          <a
+            href="https://youtube.com"
+            aria-label="Go to YouTube page"
+            target="_blank"
+            rel="noopener"
+          >
+            <YouTubeIcon className="footer-icon" />
+          </a>
+        </Grid>
         <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </Box>
