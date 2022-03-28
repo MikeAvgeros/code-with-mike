344480bde134064b2c8bc7be6108fd9a9de7a786
Mike Avgeros <mike.avgeros@gmail.com>
2022-03-28 21:37:39 +0100

    Fix incorrect base url for the api

--- client/src/components/Api/Api.js
+++ client/src/components/Api/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import store from "../Store/Store";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://codewithmike.herokuapp.com/api/",
 });
 
 export const getCourses = async () => {
