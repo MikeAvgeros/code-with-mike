da9abe0477041fea1eb143bb356a7aa06a044c8c
Mike Avgeros <mike.avgeros@gmail.com>
2022-03-31 19:43:53 +0100

    Update styling on order item in orders history page.

--- client/src/components/Profile/OrderItem.js
+++ client/src/components/Profile/OrderItem.js
@@ -9,6 +9,7 @@ import {
   Avatar,
   Button,
   Stack,
+  CardActions,
 } from "@mui/material";
 
 const OrderItem = ({ order }) => {
@@ -32,85 +33,72 @@ const OrderItem = ({ order }) => {
         }}
       />
       <CardContent>
-        {order.items &&
-          order.items.map((item, i) => (
-            <Grid
-              container
-              key={i}
-              sx={{ mb: 2 }}
-              spacing={2}
-              direction="column"
-              alignItems="center"
-            >
-              <Grid item>
-                <Avatar alt={item.item.name} src={item.item.image} />
-              </Grid>
-              <Grid item>
-                <p style={{ width: "250px", textAlign: "center" }}>{item.item.name}</p>
-              </Grid>
-              <Grid item>
-                <p>Date of Purchase: {order.created_at.split("T")[0]}</p>
-              </Grid>
-              <Grid item>
-                <p>Payment Status: {order.payment_status}</p>
-              </Grid>
-              <Grid item>
-                <p>Quantity: {item.quantity}</p>
-              </Grid>
-              <Grid item>
-                <p>Total Price: £{totalAmount}</p>
-              </Grid>
-              <Grid item>
-                <p>VAT: £{vat}</p>
-              </Grid>
-              <Grid item>
-                <p style={{ fontWeight: "bold" }}>Grand Total: £{totalAmount + vat}</p>
-              </Grid>
-              <Grid item>
-                {order.payment_status === "Success" ? (
-                  <Stack direction="row" spacing={2}>
-                    <Link
-                      to="/"
-                      style={{ textDecoration: "none" }}
-                    >
-                      <Button size="small" className="btn">
-                        View Course
-                      </Button>
-                    </Link>
-                    <Link
-                      to={`/review/send/${order.id}`}
-                      style={{ textDecoration: "none" }}
-                    >
-                      <Button size="small" className="btn">
-                        Send review
-                      </Button>
-                    </Link>
+        <Stack direction="column" spacing={2}>
+          {order.items &&
+            order.items.map((item, i) => (
+              <Grid container key={i} direction="column">
+                <Grid item>
+                  <Stack direction="row" spacing={1}>
+                    <Avatar alt={item.item.name} src={item.item.image} />
+                    <p style={{ alignSelf: "center" }}>{item.item.name}</p>
                   </Stack>
-                ) : (
-                  <Stack direction="row" spacing={2}>
-                    <Link to="/checkout" style={{ textDecoration: "none" }}>
-                      <Button
-                        size="small"
-                        className="btn"
-                        onClick={handleRetryPayment}
-                      >
-                        Make Payment
-                      </Button>
-                    </Link>
-                    <Link to="/contact" style={{ textDecoration: "none" }}>
-                      <Button
-                        size="small"
-                        className="btn"
-                      >
-                        Contact Us
-                      </Button>
-                    </Link>
-                  </Stack>
-                )}
+                </Grid>
+                <Grid item>
+                  <p>Quantity: {item.quantity}</p>
+                </Grid>
+                <Grid item>
+                  <p>Total Price: £{totalAmount.toFixed(2)}</p>
+                </Grid>
+                <Grid item>
+                  <p>VAT: £{vat.toFixed(2)}</p>
+                </Grid>
               </Grid>
-            </Grid>
-          ))}
+            ))}
+          <p>Date of Purchase: {order.created_at.split("T")[0]}</p>
+          <p>Payment Status: {order.payment_status}</p>
+          <p style={{ fontWeight: "bold" }}>
+            Grand Total: £{(totalAmount + vat).toFixed(2)}
+          </p>
+        </Stack>
       </CardContent>
+      <CardActions>
+        <Grid item>
+          {order.payment_status === "Success" ? (
+            <Stack direction="row" spacing={2}>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                <Button size="small" className="btn">
+                  View Content
+                </Button>
+              </Link>
+              <Link
+                to={`/review/send/${order.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <Button size="small" className="btn">
+                  Send review
+                </Button>
+              </Link>
+            </Stack>
+          ) : (
+            <Stack direction="row" spacing={2}>
+              <Link to="/checkout" style={{ textDecoration: "none" }}>
+                <Button
+                  size="small"
+                  className="btn"
+                  onClick={handleRetryPayment}
+                >
+                  Make Payment
+                </Button>
+              </Link>
+              <Link to="/contact" style={{ textDecoration: "none" }}>
+                <Button size="small" className="btn">
+                  Contact Us
+                </Button>
+              </Link>
+            </Stack>
+          )}
+        </Grid>
+      </CardActions>
     </Card>
   );
 };
